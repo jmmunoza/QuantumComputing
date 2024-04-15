43b3d00569f1fda073a7098ba9c18f4b583e2a8b
jmmunoza <jmmunoza@eafit.edu.co>
2024-04-15 16:12:40 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit
+
+# Build a quantum circuit
+circuit = QuantumCircuit(3, 3)
+ 
+circuit.x(1)
+circuit.h(range(3))
+circuit.cx(0, 1)
+circuit.measure(range(3), range(3));
+
+figure = circuit.draw(output='mpl')
+plt.show()
