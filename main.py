72f59fa9cbe7aaa905b72a46ab68b7c8135b6764
jmmunoza <jmmunoza@eafit.edu.co>
2024-04-20 10:09:58 -0500

    Update Grover algorithm in main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from src.algorithms.grover.grover import Grover
 from src.algorithms.grover.oracle import Oracle
 
 if __name__ == '__main__':
-    circuit = Grover(Oracle(13), 4)
-    print(circuit)
-    circuit.run()
-    print(circuit)
-    circuit.draw()
+    grover = Grover(Oracle(63), 6)
+    grover.run()
+    grover.draw()
