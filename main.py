b049874f4bc4f0963e2910e206b1a63ac991f9c4
jmmunoza <jmmunoza@eafit.edu.co>
2024-05-26 11:59:13 -0500

    Update main.py and remove unused QRegister class

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from src.algorithms.grover.grover import Grover
 from src.algorithms.grover.oracle import Oracle
 
+"""
+    Para ejecutar el programa, se debe correr el archivo main.py
+    que se encuentra en la raíz del proyecto.
+    
+    Pero, antes que todo es necesario instalar las dependencias del proyecto,
+    las cuales se encuentran en el archivo requirements.txt
+    
+    Numpy fue usado para el manejo de matrices y vectores, y Matplotlib para
+    la visualización de los resultados.
+"""
 if __name__ == '__main__':
     # La función oráculo se encuentra fijada en 20, se creará un 
     # circuito de Grover con 6 qubits
